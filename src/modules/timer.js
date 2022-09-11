041884cbdb725e8f58994c6661e3764bcfee20a4
0tohik0 <71322445+0tohik0@users.noreply.github.com>
2022-09-12 00:10:08 +0300

    feat: add initial timer version

---
+++ src/modules/timer.js
@@ -0,0 +1,17 @@
+const timer = () => {
+    const timerHours = document.getElementById('timer-hours')
+    const timerMinutes = document.getElementById('timer-minutes')
+    const timerSecond = document.getElementById('timer-second')
+
+    console.log(timerHours);
+    console.log(timerMinutes);
+    console.log(timerSecond);
+}
+
+export default  timer
+
+
+
+// timer-hours
+// timer-minutes
+// timer-second
---
+++ src/modules/two.js
@@ -0,0 +1,5 @@
+const two = () => {
+    console.log('hi');
+}
+
+export default two
